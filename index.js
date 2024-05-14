c6be96207f1d229f2f6d8bcf12e43e8b7274885b
Valentín Peñalver <valenpga@gmail.com>
2024-05-14 13:14:16 +0200

    add new confuration database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const dbConnection = require('./config/config')
 const app = express()
 require('dotenv').config()
 PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => (res.send('Poyecto Back')))
+
+dbConnection()
 
 app.listen(PORT, () => {
     console.log(`Express esta corriendo en el puerto http://localhost:${PORT}`)
